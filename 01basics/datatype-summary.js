96360ee5942fdd2aae6f876b9a312f6ad33ccdbc
yash190101 <96413827+yash190101@users.noreply.github.com>
2023-10-23 07:18:00 +0000

    datatypes basics covered

---
+++ 01basics/datatype-summary.js
@@ -0,0 +1,74 @@
+// //  Primitive
+
+// //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// // const bigNumber = 3456543576654356754n
+
+
+
+// // Reference (Non primitive)
+
+// // Array, Objects, Functions
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof anotherId);
+
+// // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+let number = 10;
+let str = "Yash"
+let isTrue = true;
+let undefi;
+let noValue = null;
+let bigNumber = 5562254125n
+
+
+
+console.log(typeof number);
+console.log(typeof str);
+console.log(typeof isTrue);
+console.log(typeof undefi);
+console.log(typeof noValue);
+console.log(typeof bigNumber);
+
+
+
+let arr = [10,"yash",true]
+
+let myFunction = function(){
+    console.log("Hello World");
+}
+
+let obj = {
+    "name" : "Yash",
+    "number" : 10
+}
+
+
+
+console.log(typeof arr);
+console.log(typeof myFunction);
+console.log(typeof obj);
