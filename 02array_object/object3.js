173206496cb03505112d113e29fcc4d2b012194f
yash190101 <96413827+yash190101@users.noreply.github.com>
2023-10-25 05:52:46 +0000

    destructuring of obj and json

---
+++ 02array_object/object3.js
@@ -0,0 +1,18 @@
+let user = {
+    instructorName : "Yash",
+    number : 10,
+    email : "Yvpatil"
+}
+//destructuring of an object
+let {instructorName : name} = user
+let name1 = user["instructorName"];
+// console.log(name)
+// console.log(name1)
+
+name = "Dhanesh"
+console.log(user);
+console.log(name)
+
+name1 = "Dhanesh"
+console.log(user);
+console.log(name1)
