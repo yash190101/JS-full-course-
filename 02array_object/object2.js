ca1f93ced53929991ab4fd3eaa1eda859167a554
yash190101 <96413827+yash190101@users.noreply.github.com>
2023-10-25 05:17:50 +0000

    object part 2

---
+++ 02array_object/object2.js
@@ -0,0 +1,11 @@
+let user = {
+    name : "Yash",
+    number : 10,
+    email : "Yvpatil"
+}
+//Object.freeze(user) //This method is used to freexe a object
+
+console.log(Object.keys(user));//to get array of keys of obj
+console.log(Object.values(user)); //to get array of values of obj
+console.log(Object.entries(user)); //to get the key value pairs of object properties
+// console.log(user)
