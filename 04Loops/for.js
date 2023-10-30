277a0c6c909193e3a0f47c7f7404d1658c9a258e
yash190101 <96413827+yash190101@users.noreply.github.com>
2023-10-30 08:29:16 +0000

    all types of for

---
+++ 04Loops/for.js
@@ -0,0 +1,82 @@
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     array.forEach(element => {
+        
+//     });
+// }
+
+
+// let index = 0
+// // while (index <= 10) {
+// //     console.log(`Value of index is ${index}`);
+// //     index = index + 2
+// // }
+
+// let myArray = ['flash', "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     //console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// let score = 100
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
+
+var map = new Map();
+map.set("one","1")
+map.set("two","2")
+map.set("three","3")
+
+var obj = {
+    "one" : 1,
+    "two" : 2,
+    "three" : 3
+}
+// for of is usde for map 
+
+// for (const [key,value] of map) {
+
+//     console.log(`Key is ${key} value is ${value}`);
+// }
+
+// for in is used for object 
+
+// for (const key in obj) {
+
+//     console.log(obj[key]);
+
+// }
+
+// for (const key in map) {
+ 
+//     console.log(key);
+
+// }
+
+
+// const arr1 = [1,2,3,4,5]
+
+// const filteredArray = []
+
+//foreach use karke filtering the array
+
+// arr1.forEach((item) => {
+//     if(item > 2){
+//         filteredArray.push(item)
+//     }
+// })
+
+// console.log(filteredArray);
+
+
+//Using filter for filtering
+
+// const newArray = arr1.filter((item) => item > 2) // this is using implicit return
+
+// console.log(newArray);
+
