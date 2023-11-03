5243d2166815bb4d241d1d46e7ca94e04ffb87b4
yash190101 <96413827+yash190101@users.noreply.github.com>
2023-11-03 09:50:22 +0000

    doubled starting problem solved

--- 05Projects/06_autoColorChanger/index.js
+++ 05Projects/06_autoColorChanger/index.js
@@ -1,18 +1,22 @@
-let setColor;
+
 let setTime;
 function getRandomColor(){
-    setColor ='#'+Math.floor(Math.random()*16777215).toString(16);
+    let setColor ='#'+Math.floor(Math.random()*16777215).toString(16);
     document.querySelector("body").style.backgroundColor = setColor
 }
+
 document.querySelector("#start").addEventListener("click",function(){
-    setTime = setInterval(function(){
-        getRandomColor();
-        console.log("Color Changed");
-    }, 1000);
+    if(!setTime){    
+        setTime = setInterval(function(){
+            getRandomColor();
+            console.log("Color Changed");
+        }, 1000);
+    }
 })
 
 
 document.getElementById("stop").addEventListener("click",function(){
     clearInterval(setTime);
+    setTime = undefined
     console.log("Stopped");
 })
